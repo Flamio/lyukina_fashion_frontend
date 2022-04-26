b5cea90b3e5976584a7c8ed03f0f750c76809e38
Maksim Menshikov <maksim-menshikov@mail.ru>
2022-04-26 15:34:43 +0400

    merge products in cart by quantity

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -1,12 +1,22 @@
 import { CartConstants } from "../constants"
 
 export const CartReducer = (state = {products: []}, action) => {
+
+  const mergeProductByQuantuty = (product, index, productExistIndex) => {
+    if (index === productExistIndex) {
+      product.quantity = product.quantity + action.product.quantity
+    }
+    return product;
+  }
+
     switch (action.type) {
         case CartConstants.PUT_PRODUCT:
-            console.log("add to cart")
+          const productExistIndex = state.products.findIndex(p => p.id === action.product.id && p.size === action.product.size)            
+          console.log(productExistIndex)
           return {
               ...state,
-              products: state.products.concat(action.product)
+              products: productExistIndex !== -1 ? 
+                state.products.map((p, index) => mergeProductByQuantuty(p, index, productExistIndex)) : state.products.concat(action.product)
           }
     
         default:
