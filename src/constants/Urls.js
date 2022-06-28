dc1424bb604b192ef2cb1eaf28f7ae28b13de966
Maksim Menshikov <maksim-menshikov@mail.ru>
2022-06-28 09:37:01 +0400

    urls

--- src/constants/Urls.js
+++ src/constants/Urls.js
@@ -1,5 +1,5 @@
 export const Urls = {
-  MAIN_PAGE_SERVICE_URL: "/api/main",
-  PRODUCT_SERVICE_URL: "/api/products",
-  SIZE_SERVICE_URL: "/api/sizes"
+  MAIN_PAGE_SERVICE_URL: "/api/front/main",
+  PRODUCT_SERVICE_URL: "/api/front/products",
+  SIZE_SERVICE_URL: "/api/front/sizes"
 }
